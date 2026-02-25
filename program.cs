5db4d2610f63eccc4be4d0c4470c7328fbf17aa6
yaoshikuro <yukivoron@gmail.com>
2026-02-25 10:44:55 +0300

    Added hello fio

--- program.cs
+++ program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello, World!");
+Console.WriteLine("Аббасов Салман Сананович");
+Console.WriteLine("ИСП-233");
+Console.WriteLine(DateTime.Now);

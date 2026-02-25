a4156f3ca05751b50e8ec530cb3309a8bb8dde52
yaoshikuro <yukivoron@gmail.com>
2026-02-25 10:54:42 +0300

    added menu

--- program.cs
+++ program.cs
@@ -1,4 +1,25 @@
-﻿Console.WriteLine("Hello, World!");
-Console.WriteLine("Аббасов Салман Сананович");
-Console.WriteLine("ИСП-233");
-Console.WriteLine(DateTime.Now);
+﻿
+using System.Linq.Expressions;
+
+string hello = "Hello, World!";
+string fio = "Аббасов Салман Сананович";
+string gruppa = "ИСП-233";
+string data = "25.02.2026";
+
+int q = Convert.ToInt32(Console.ReadLine());
+if (q == 1)
+{
+    Console.WriteLine(hello);
+}
+if (q == 2)
+{
+    Console.WriteLine(fio);
+}
+if (q == 3)
+{
+    Console.WriteLine(gruppa);
+}
+if (q == 4)
+{
+    Console.WriteLine(data);
+}

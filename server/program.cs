24009737749ea03211a64d7e67a4943b655f4b7a
yaoshikuro <yukivoron@gmail.com>
2026-02-25 10:59:27 +0300

    Added logs and data

---
+++ server/program.cs
@@ -0,0 +1,25 @@
+﻿
+using System.Linq.Expressions;
+
+string hello = "Hello, World!";
+string fio = "Аббасов Салман Сананович";
+string gruppa = "ИСП-233";
+string data = "25.02.2026";
+
+int q = Convert.ToInt32(Console.ReadLine());
+if (q == 1)
+{
+    Console.WriteLine(hello);
+}
+if (q == 2)
+{
+    Console.WriteLine(fio);
+}
+if (q == 3)
+{
+    Console.WriteLine(gruppa);
+}
+if (q == 4)
+{
+    Console.WriteLine(data);
+}
